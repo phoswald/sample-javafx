249e0b7d938ea7fbb38fe6ea7dcaa83afc2f725c
Philip Oswald <philip.oswald@gmail.com>
2015-08-28 00:00:16 +0200

    Private fields and handlers of controller, using annotations

--- src/main/java/phoswald/MyController.java
+++ src/main/java/phoswald/MyController.java
@@ -4,20 +4,23 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 
 
 public class MyController implements Initializable {
 
-    public void handleTheButton(ActionEvent event) {
-        System.out.println("You clicked me (1)!");
+    @FXML
+    private void handleButtonOne(ActionEvent event) {
+        System.out.println("You clicked me (one)!");
     }
 
-    public Button button2;
+    @FXML
+    private Button button2;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        button2.setOnAction(ev -> System.out.println("You clicked me (2)!"));
+        button2.setOnAction(ev -> System.out.println("You clicked me (two)!"));
     }
 }

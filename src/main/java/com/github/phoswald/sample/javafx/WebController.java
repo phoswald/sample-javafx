48a0ec88a806641bec0c22d18b07c71f6f3e5615
Philip Oswald <philip.oswald@gmail.com>
2024-06-22 16:39:25 +0200

    Experiments with WebView

--- src/main/java/com/github/phoswald/sample/javafx/WebController.java
+++ src/main/java/com/github/phoswald/sample/javafx/WebController.java
@@ -1,17 +1,23 @@
 package com.github.phoswald.sample.javafx;
 
+import java.io.StringReader;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.w3c.dom.DOMImplementation;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
+import org.w3c.dom.bootstrap.DOMImplementationRegistry;
 import org.w3c.dom.events.Event;
 import org.w3c.dom.events.EventListener;
 import org.w3c.dom.events.EventTarget;
 import org.w3c.dom.html.HTMLInputElement;
+import org.w3c.dom.ls.DOMImplementationLS;
+import org.w3c.dom.ls.LSInput;
+import org.w3c.dom.ls.LSParser;
 
 import com.github.phoswald.sample.Application;
 import com.github.phoswald.sample.ApplicationModule;
@@ -59,7 +65,7 @@ public class WebController implements Initializable {
                     <p>Name: <input type="text" id="input"></p>
                     <p><a href="#" onclick="link1.invoke();">Link 1</a></p>
                     <p><a href="#" id="link2">Link 2</a></p>
-                    <p>Message: <span id="output">???</span</p>
+                    <p>Message: <span id="output">???</span></p>
                   </body>
                 </html>
                 """;
@@ -85,7 +91,7 @@ public class WebController implements Initializable {
 
     private void setElementInnerText(String elementId, String text) {
         Element elementNode = documentNode.getElementById(elementId);
-        if(elementNode != null) {
+        if (elementNode != null) {
             Node childNode;
             while ((childNode = elementNode.getFirstChild()) != null) {
                 elementNode.removeChild(childNode);
@@ -93,10 +99,62 @@ public class WebController implements Initializable {
             elementNode.appendChild(documentNode.createTextNode(text));
         }
     }
+
+//  private void setElementInnerHtml(String elementId, String html) {
+//      Element elementNode = documentNode.getElementById(elementId);
+//      if(elementNode instanceof ElementImpl elementNodeImpl) { // com.sun.webkit.dom.ElementImpl
+//          elementNodeImpl.setInnerHTML(html); 
+//      }
+//  }
+
+    private void setElementInnerHtml(String elementId, String html) {
+      Element elementNode = documentNode.getElementById(elementId);
+      if(elementNode != null) {
+          Node childNode;
+          while ((childNode = elementNode.getFirstChild()) != null) {
+              elementNode.removeChild(childNode);
+          }
+          Node innerNode = parseHtml(html); // TODO: why does cloneNode() not work here?
+          for(int item = 0; item < innerNode.getChildNodes().getLength(); item++) {
+              elementNode.appendChild(cloneNode(innerNode.getChildNodes().item(item)));
+          }
+      }
+    }
     
+    private Element parseHtml(String html) {
+        try {
+//          DOMImplementation domImpl = documentNode.getImplementation();
+            DOMImplementation domImpl = DOMImplementationRegistry.newInstance().getDOMImplementation("");
+            DOMImplementationLS domImplLS = (DOMImplementationLS) domImpl.getFeature("LS", "3.0");
+            LSParser parser = domImplLS.createLSParser(DOMImplementationLS.MODE_SYNCHRONOUS, null);
+            LSInput input = domImplLS.createLSInput();
+            input.setCharacterStream(new StringReader("<html xmlns='http://www.w3.org/1999/xhtml'><body>" + html + "</body></html>"));
+            Document innerDocument = parser.parse(input);
+            return (Element) innerDocument.getElementsByTagName("body").item(0);
+            
+        } catch (ReflectiveOperationException e) {
+            throw new IllegalStateException(e);
+        }
+    }
+    
+    private Node cloneNode(Node inputNode) {
+        switch(inputNode.getNodeType()) {
+            case Node.ELEMENT_NODE:
+                Node outputNode = documentNode.createElementNS(inputNode.getNamespaceURI(), inputNode.getLocalName());
+                for(int item = 0; item < inputNode.getChildNodes().getLength(); item++) {
+                    outputNode.appendChild(cloneNode(inputNode.getChildNodes().item(item)));
+                }
+                return outputNode;
+            case Node.TEXT_NODE:
+                return documentNode.createTextNode(inputNode.getTextContent());
+            default:
+                throw new UnsupportedOperationException();
+        }
+    }
+
     private String getInputValue(String elementId) {
         Element elementNode = documentNode.getElementById(elementId);
-        if(elementNode instanceof HTMLInputElement inputElementNode) {
+        if (elementNode instanceof HTMLInputElement inputElementNode) {
             return inputElementNode.getValue();
         } else {
             return null;
@@ -108,7 +166,7 @@ public class WebController implements Initializable {
     }
 
     public void onLink2(Event evt) {
-        setElementInnerText("output", "Hallo, " + getInputValue("input") + "!");
+        setElementInnerHtml("output", "Hallo, <b>" + getInputValue("input") + "</b>!");
     }
 
     public static record InvokeRunnable(Runnable runnable) {
